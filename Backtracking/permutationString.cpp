b95085b461d044b30dad93f912aefba52981cfec
shubham-99 <shubham@flixstock.com>
2023-07-09 12:35:19 +0530

    change string premutation to integer permutations

--- Backtracking/permutationString.cpp
+++ Backtracking/permutationString.cpp
@@ -1,28 +1,39 @@
 #include<iostream>
 #include<string>
 #include<algorithm>
+#include<vector>
 using namespace std;
 
-void permutation(string str, int i)
+void permutation(vector<int> num, int i,vector<vector<int>> &store)
 {
-    if(i==str.length())
+    if(i==num.size())
     {
-        cout<<str<<endl;
+        store.push_back(num);
         return;
     }
-    for(int j=i;j<str.length();j++)
+    for(int j=i;j<num.size();j++)
     {
-        swap(str[i],str[j]);
-        permutation(str,i+1);
-        swap(str[i],str[j]);
+        swap(num[i],num[j]);
+        permutation(num,i+1, store);
+        swap(num[i],num[j]);
     }
 
 }
 
 int main()
 {
-    string str= "ABC";
-    permutation(str,0);
+    vector<int> num={1,2,3};
+    vector<vector<int>> store;
+
+    permutation(num,0,store);
+    for(vector<int> num : store)
+    {
+        for(int i : num)
+        {
+            cout<<i<< " ";
+        }
+        cout<<endl;
+    }
     cin.get();
     return 0;
 }
